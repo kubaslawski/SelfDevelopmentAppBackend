ecd6f9df951b4586044187244c1c62893c720e99
Kuba Ślawski <kubaslawski.it@gmail.com>
2026-02-08 21:45:54 +0100

    feat: added url with privacy policy html

--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
@@ -29,6 +30,12 @@ class EmailTokenObtainPairView(TokenObtainPairView):
 urlpatterns = [
     # Admin
     path("admin/", admin.site.urls),
+    # Static pages
+    path(
+        "privacy-policy/",
+        TemplateView.as_view(template_name="privacy_policy.html"),
+        name="privacy_policy",
+    ),
     # =========================================================================
     # Authentication (Email-based login for OAuth flow)
     # =========================================================================
