bfa0c6b89089395fdf477598f4a1b0081abf8ef7
Kuba Ślawski <kubaslawski.it@gmail.com>
2026-04-13 19:32:05 +0200

    chore: configured cors headers

--- config/settings/base.py
+++ config/settings/base.py
@@ -183,7 +183,7 @@ SPECTACULAR_SETTINGS = {
 # CORS settings
 CORS_ALLOWED_ORIGINS = config(
     "CORS_ALLOWED_ORIGINS",
-    default="http://localhost:3000,http://127.0.0.1:3000",
+    default="http://localhost:5174,http://127.0.0.1:5174",
     cast=lambda v: [s.strip() for s in v.split(",")],
 )
 
